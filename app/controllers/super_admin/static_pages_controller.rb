0ec0d654e038f561f280d05f94760176f0a7f004
Quentin Sonrel <sudiukil@gmx.fr>
2018-05-17 15:26:42 +0200

    Updated Static Pages controller to make the form functional

--- app/controllers/super_admin/static_pages_controller.rb
+++ app/controllers/super_admin/static_pages_controller.rb
@@ -10,49 +10,53 @@ module SuperAdmin
       render(:index, locals: { static_pages: @static_pages.page(1) })
     end
 
-    # GET /static_pages/1
-    # GET /static_pages/1.json
-    def show; end
-
     # GET /static_pages/new
     def new
+      authorize(StaticPage)
       @static_page = StaticPage.new
+
       Language.all.each do |l|
-        @static_page.static_page_contents << StaticPageContent.new(content: '', language: l)
+        @static_page.contents.new(language: l)
       end
     end
 
     # GET /static_pages/1/edit
-    def edit; end
+    def edit
+      authorize(StaticPage)
+    end
 
     # POST /static_pages
     # POST /static_pages.json
     def create
-      @static_page = StaticPage.new(static_page_params)
+      authorize(StaticPage)
 
-      respond_to do |format|
-        if @static_page.save
-          format.html { redirect_to @static_page, notice: 'Static page was successfully created.' }
-          format.json { render :show, status: :created, location: @static_page }
-        else
-          format.html { render :new }
-          format.json { render json: @static_page.errors, status: :unprocessable_entity }
-        end
+      begin
+        @static_page = StaticPage.create!(static_page_params)
+        flash[:notice] = _('Static Page created successfully')
+      rescue ActionController::ParameterMissing
+        flash[:alert] = _('Unable to save since static_page parameter is missing')
+      rescue ActiveRecord::RecordInvalid => e
+        flash[:alert] = e.message
       end
+
+      redirect_to action: :index
     end
 
     # PATCH/PUT /static_pages/1
     # PATCH/PUT /static_pages/1.json
     def update
-      respond_to do |format|
-        if @static_page.update(static_page_params)
-          format.html { redirect_to @static_page, notice: 'Static page was successfully updated.' }
-          format.json { render :show, status: :ok, location: @static_page }
-        else
-          format.html { render :edit }
-          format.json { render json: @static_page.errors, status: :unprocessable_entity }
-        end
+      authorize(StaticPage)
+
+      begin
+        @static_page.update!(static_page_params)
+        flash[:notice] = _('Static Page updated successfully')
+      rescue ActionController::ParameterMissing
+        flash[:alert] = _('Unable to save since static_page parameter is missing')
+      rescue ActiveRecord::RecordInvalid => e
+        flash[:alert] = e.message
       end
+
+      redirect_to action: :index
     end
 
     # DELETE /static_pages/1
@@ -79,7 +83,11 @@ module SuperAdmin
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def static_page_params
-      params.require(:static_page).permit(:name, :url)
+      params.require(:static_page).permit(
+        :name,
+        :url,
+        static_page_contents_attributes: [%i[id language_id content]]
+      )
     end
   end
 end
--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -1,7 +1,9 @@
 # Static page class
 class StaticPage < ActiveRecord::Base
-  has_many :static_page_contents
-  accepts_nested_attributes_for :static_page_contents
+  has_many :static_page_contents, dependent: :destroy
+  accepts_nested_attributes_for :static_page_contents, allow_destroy: true
 
   validates :name, :url, presence: true, uniqueness: true
+
+  alias contents static_page_contents
 end
