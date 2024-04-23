19a559e15766bcf18b0fab8b5ffa8ec18c8b5474
Benjamin FAURE <benjamin.faure@inist.fr>
2024-04-23 11:23:03 +0200

    feat: views & controllers first draft

---
+++ engines/madmp_opidor/app/controllers/super_admin/template_mappings_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module SuperAdmin
+  # Controller for managing MadmpSchemas
+  class TemplateMappingsController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+  end
+end
