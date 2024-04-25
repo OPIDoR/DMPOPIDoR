7f90512003495eab6d64cd49268223a700b04550
Benjamin FAURE <benjamin.faure@inist.fr>
2024-04-25 16:37:22 +0200

    feat: added template data_type support to question form & questions_controller

---
+++ app/models/settings/question.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: settings
+#
+#  id          :integer          not null, primary key
+#  target_type :string           not null
+#  value       :text
+#  var         :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  target_id   :integer          not null
+#
+# Indexes
+#
+#  settings_target_type_target_id_var_key  (target_type,target_id,var) UNIQUE
+#
+
+module Settings
+  # Settings the questions
+  class Question < RailsSettings::SettingObject
+    AVAILABLE_CLASSNAMES = {
+      "none" => %w[
+        research_output_description
+        data_reuse
+        personal_data_issues
+        legal_issues
+        ethical_issues
+        data_collection
+        data_processing
+        data_storage
+        documentation_quality
+        quality_assurance_method
+        data_sharing
+        data_preservation
+      ],
+      "software" => %w[
+        software_description
+      ]
+    }.freeze
+  end
+end
