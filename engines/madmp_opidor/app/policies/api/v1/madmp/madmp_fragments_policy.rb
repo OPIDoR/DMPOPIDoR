4b64e77f1521700a2820ecc89f95c5fe4c29b75d
Benjamin FAURE <benjamin.faure@inist.fr>
2024-05-27 14:35:27 +0200

    fix: added missing access check for ApiClient to MadmpFragmentsController#show route

--- engines/madmp_opidor/app/policies/api/v1/madmp/madmp_fragments_policy.rb
+++ engines/madmp_opidor/app/policies/api/v1/madmp/madmp_fragments_policy.rb
@@ -23,7 +23,8 @@ module Api
             plan = @fragment.plan
             plan.readable_by?(@client.id)
           else
-            true
+            plan = @fragment.plan
+            plan.readable_by_client?(@client.id)
           end
         end
 
@@ -32,7 +33,8 @@ module Api
             plan = @fragment.plan
             plan.readable_by?(@client.id)
           else
-            true
+            plan = @fragment.plan
+            plan.readable_by_client?(@client.id)
           end
         end
 
@@ -41,7 +43,7 @@ module Api
             plan = @fragment.plan
             plan.editable_by?(@client.id)
           else
-            true
+            false
           end
         end
       end
