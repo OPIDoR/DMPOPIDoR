f0b724fd276aee1a5d349518a74295a449874526
Hugo COLLIN <hugo.collin@inist.fr>
2024-06-10 10:43:29 +0200

    docs: changing comments

--- engines/madmp_opidor/app/controllers/super_admin/template_mappings_controller.rb
+++ engines/madmp_opidor/app/controllers/super_admin/template_mappings_controller.rb
@@ -8,7 +8,7 @@ module SuperAdmin
     respond_to :json, :html
     skip_before_action :verify_authenticity_token, only: %i[create update destroy] # TODO: Not a good practice, CSRF issues may arise
 
-    # GET /dmp_mapping
+    # GET /super_admin/template_mappings
     def index
       @mappings = DmpMapping.all
       respond_to do |format|
@@ -20,7 +20,7 @@ module SuperAdmin
       end
     end
 
-    # GET /dmp_mapping/:id
+    # GET /super_admin/template_mappings/:id
     def show
       mapping = DmpMapping.find_by(id: params[:id])
       if mapping
@@ -30,7 +30,7 @@ module SuperAdmin
       end
     end
 
-    # POST /dmp_mapping
+    # POST /super_admin/template_mappings
     def create
       mapping = DmpMapping.new(mapping_params)
       if mapping.save
@@ -40,7 +40,7 @@ module SuperAdmin
       end
     end
 
-    # PATCH/PUT /dmp_mapping/:id
+    # PATCH/PUT /super_admin/template_mappings/:id
     def update
       mapping = DmpMapping.find_by(id: params[:id])
       if mapping&.update(mapping_params)
@@ -50,7 +50,7 @@ module SuperAdmin
       end
     end
 
-    # DELETE /dmp_mapping/:id
+    # DELETE /super_admin/template_mappings/:id
     def destroy
       mapping = DmpMapping.find_by(id: params[:id])
       if mapping&.destroy
@@ -65,77 +65,5 @@ module SuperAdmin
     def mapping_params
       params.require(:dmp_mapping).permit(:type_mapping, :source_id, :target_id, :name, mapping: {})
     end
-
-    # # GET /dmp_mapping/:id
-    # def show
-    #   @mapping = DmpMapping.find_by(id: params[:id])
-    #   respond_to do |format|
-    #     format.json do
-    #       if @mapping
-    #         render json: { message: "DMPOPDIoR mapping for [#{params[:id]}]", **@mapping.as_json }, status: :ok
-    #       else
-    #         render json: { message: 'Mapping not found' }, status: :not_found
-    #       end
-    #     end
-    #   end
-    # end
-
-    # # POST /dmp_mapping
-    # def create
-    #   @mapping = DmpMapping.new(mapping_params)
-    #   if @mapping.save
-    #     respond_to do |format|
-    #       format.json { render json: { message: 'Mapping created', **@mapping.as_json }, status: :created }
-    #     end
-    #   else
-    #     respond_to do |format|
-    #       format.json do
-    #         render json: { message: 'Error creating mapping', errors: @mapping.errors.full_messages },
-    #                status: :bad_request
-    #       end
-    #     end
-    #   end
-    # end
-
-    # # PATCH/PUT /dmp_mapping/:id
-    # def update
-    #   @mapping = DmpMapping.find_by(id: params[:id])
-    #   if @mapping&.update(mapping_params)
-    #     respond_to do |format|
-    #       format.html { redirect_to some_path, notice: 'Mapping updated' }
-    #       format.json { render json: { message: 'Mapping updated', **@mapping.as_json }, status: :ok }
-    #     end
-    #   else
-    #     respond_to do |format|
-    #       format.html { render :edit }
-    #       format.json do
-    #         render json: { message: 'Error updating mapping', errors: @mapping.errors.full_messages },
-    #                status: :bad_request
-    #       end
-    #     end
-    #   end
-    # end
-
-    # # DELETE /dmp_mapping/:id
-    # def destroy
-    #   @mapping = DmpMapping.find_by(id: params[:id])
-    #   if @mapping&.destroy
-    #     respond_to do |format|
-    #       format.html { redirect_to some_path, notice: 'Mapping deleted' }
-    #       format.json { render json: { message: 'Mapping deleted' }, status: :ok }
-    #     end
-    #   else
-    #     respond_to do |format|
-    #       format.html { redirect_to some_path, alert: 'Error deleting mapping' }
-    #       format.json { render json: { message: 'Error deleting mapping' }, status: :bad_request }
-    #     end
-    #   end
-    # end
-
-    # private
-
-    # def mapping_params
-    #   params.require(:dmp_mapping).permit(:type_mapping, :source_id, :target_id, :name, mapping: {})
-    # end
   end
 end
