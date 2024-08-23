5a95b14ca400a820665c4ce4305419171b55d633
Benjamin FAURE <benjamin.faure@inist.fr>
2024-08-23 10:37:56 +0200

    refactor: madmp_fragment is now created when added a comment on an unanswered question

---
+++ app/models/dmpopidor/answer.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Dmpopidor
+  # Customized code for Answer model
+  module Answer
+    def instantiate_fragment
+      if plan.template.structured? && madmp_fragment.nil?
+        dmp_id = plan.json_fragment.id
+        madmp_schema = MadmpSchema.find(question.madmp_schema_id)
+        defaults = madmp_schema.defaults(plan.template.locale)
+        madmp_fragment = MadmpFragment.create!(
+          data: {},
+          answer_id: id,
+          parent_id: research_output.present? ? research_output.json_fragment.id : nil,
+          dmp_id:,
+          madmp_schema: madmp_schema,
+          classname: madmp_schema.classname,
+          additional_info: {
+            'property_name' => madmp_schema.property_name_from_classname
+          }
+        )
+        madmp_fragment.instantiate
+        madmp_fragment.handle_defaults(defaults)
+        madmp_fragment.id
+      end
+      nil
+    end
+  end
+end
