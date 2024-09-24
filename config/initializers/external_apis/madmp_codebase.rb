b5737de7953fcccc16a8cfb2c5d0c228bc09a30a
Benjamin FAURE <benjamin.faure@inist.fr>
2024-09-24 15:32:16 +0200

    fix: madmp_codebase run_path

--- config/initializers/external_apis/madmp_codebase.rb
+++ config/initializers/external_apis/madmp_codebase.rb
@@ -4,5 +4,5 @@
 # MADMP Codebase Service
 Rails.configuration.x.madmp_codebase.api_base_url = ENV.fetch('MADMP_CODEBASE_API_BASE_URL', 'http://codebase_api:50000/')
 Rails.configuration.x.madmp_codebase.scripts_path = ENV.fetch('MADMP_CODEBASE_SCRIPTS_PATH', 'scripts')
-Rails.configuration.x.madmp_codebase.run_path = ENV.fetch('MADMP_CODEBASE_RUN_PATH', 'scripts/%s/run')
+Rails.configuration.x.madmp_codebase.run_path = ENV.fetch('MADMP_CODEBASE_RUN_PATH', 'scripts/run')
 Rails.configuration.x.madmp_codebase.active = ENV.fetch('MADMP_CODEBASE_ACTIVE', true).to_s.casecmp('true').zero?
