7869a2cb6294e28592911769ef128e2d76be43c3
wilmouths <wilmouthsteven@gmail.com>
2024-10-10 14:38:51 +0200

    chore: adding canal-u icon/url

--- config/initializers/_dmproadmap.rb
+++ config/initializers/_dmproadmap.rb
@@ -295,5 +295,6 @@ module DMPRoadmap
     config.x.directus.public_url = ENV.fetch('DIRECTUS_PUBLIC_URL', 'http://localhost:8080/directus')
     config.x.dmpopidor.enable_third_party_form = ENV.fetch('ENABLE_THIRD_PARTY_FORM', false)
     config.x.dmpopidor.redis_url = ENV.fetch('REDIS_URL', 'redis://default:changeme@localhost:6379/1')
+    config.x.application.canalu_url = ENV.fetch('CANALU_URL', 'https://www.canal-u.tv/chaines/inist-cnrs/opidor')
   end
 end
