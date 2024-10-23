64694f6f7dfe73fc9dbab24f32a9bae7562c2260
wilmouths <wilmouthsteven@gmail.com>
2024-10-23 16:55:12 +0200

    feat: adding filter on classname

--- engines/madmp_opidor/app/graphql/types/fragment_filter_input_type.rb
+++ engines/madmp_opidor/app/graphql/types/fragment_filter_input_type.rb
@@ -3,5 +3,6 @@ module Types
     argument :grantId, GraphQL::Types::JSON, required: false
     argument :fieldName, String, required: false
     argument :value, String, required: false
+    argument :className, String, required: false
   end
 end
