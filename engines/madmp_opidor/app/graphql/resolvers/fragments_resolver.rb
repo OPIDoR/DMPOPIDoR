a7314ee70e9324b7c27512da0914aaf06d39cd74
wilmouths <wilmouthsteven@gmail.com>
2024-10-23 16:55:29 +0200

    refactor: creating function to format and check permissions

--- engines/madmp_opidor/app/graphql/resolvers/fragments_resolver.rb
+++ engines/madmp_opidor/app/graphql/resolvers/fragments_resolver.rb
@@ -14,6 +14,10 @@ module Resolvers
         data = filter_by_grant_id(filter.grantId)
       end
 
+      if filter.className.present?
+        data = filter_by_class_name(filter.className)
+      end
+
       if filter.fieldName.present? && filter.value.present?
         data = filter_by_fieldName(filter.fieldName, filter.value)
       end
@@ -23,43 +27,33 @@ module Resolvers
 
     private
 
+    def format_fragments(fragments)
+      fragments.map do |fragment|
+        if Api::V1::Madmp::MadmpFragmentsPolicy.new(context[:current_user], fragment).show?
+          { plan: fragment.plan }.merge(fragment.plan.json_fragment.get_full_fragment)
+        end
+      end.compact.uniq { |fragment| fragment[:plan].id }
+    end
+
     def filter_by_grant_id(grant_ids)
       if grant_ids.is_a?(Hash) && grant_ids["regex"].present?
         regex = grant_ids["regex"].gsub(/\A\/|\/\z/, '')
-        fragments = MadmpFragment.where("data->>'grantId' ~* ?", regex).map do |fragment|
-          if Api::V1::Madmp::MadmpFragmentsPolicy.new(context[:current_user], fragment).show?
-            {
-              plan: fragment.plan
-            }.merge(fragment.plan.json_fragment.get_full_fragment)
-          else
-            nil
-          end
-        end.compact
+        fragments = MadmpFragment.where("data->>'grantId' ~* ?", regex)
       elsif grant_ids.is_a?(Array)
         grant_ids = grant_ids.compact.uniq
-        fragments = MadmpFragment.where("data->>'grantId' IN (?)", grant_ids).map do |fragment|
-          if Api::V1::Madmp::MadmpFragmentsPolicy.new(context[:current_user], fragment).show?
-            {
-              plan: fragment.plan
-            }.merge(fragment.plan.json_fragment.get_full_fragment)
-          else
-            nil
-          end
-        end.compact
+        fragments = MadmpFragment.where("data->>'grantId' IN (?)", grant_ids)
       end
-      fragments
+      format_fragments(fragments)
+    end
+
+    def filter_by_class_name(class_name)
+      fragments = MadmpFragment.where(classname: class_name)
+      format_fragments(fragments)
     end
 
     def filter_by_fieldName(field_name, value)
-      MadmpFragment.where("data->>? = ?", field_name, value).map do |fragment|
-        if Api::V1::Madmp::MadmpFragmentsPolicy.new(context[:current_user], fragment).show?
-          {
-            plan: fragment.plan
-          }.merge(fragment.plan.json_fragment.get_full_fragment)
-        else
-          nil
-        end
-      end.compact
+      fragments =  MadmpFragment.where("data->>? = ?", field_name, value)
+      format_fragments(fragments)
     end
   end
 end
