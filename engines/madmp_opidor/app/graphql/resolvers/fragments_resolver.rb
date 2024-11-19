f8ae72472e17daac708a953b80753b3e36271515
wilmouths <wilmouthsteven@gmail.com>
2024-11-19 14:48:41 +0100

    refactor: updating filters & resolvers

--- engines/madmp_opidor/app/graphql/resolvers/fragments_resolver.rb
+++ engines/madmp_opidor/app/graphql/resolvers/fragments_resolver.rb
@@ -1,63 +1,56 @@
 module Resolvers
   class FragmentsResolver < BaseResolver
     type [Types::FragmentType], null: false
-    argument :filter, Types::FragmentFilterInputType, required: false
 
-    def resolve(filter: nil)
-      fragments = apply_filters(filter) if filter.present?
+    argument :grantId, GraphQL::Types::JSON, required: false
+    argument :className, String, required: false
+    argument :field, Types::FieldInputType, required: false
 
-      p "===================="
-      p fragments
-      p "===================="
+    def resolve(**args)
+      filter = args
 
-      fragments
+      plan_id = object.id if object.respond_to?(:id)
+
+      fragments = plan_id ? MadmpFragment.where(dmp_id: plan_id) : MadmpFragment.all
+
+      fragments = apply_filters(fragments, filter) if filter.present? && filter.any?
+
+      format_fragments(fragments)
     end
 
-    def apply_filters(filter)
-      if filter.grantId.present?
-        data = filter_by_grant_id(filter.grantId)
+    def apply_filters(fragments, filter)
+      fragments = filter_by_grant_id(fragments, filter[:grantId]) if filter[:grantId].present?
+      fragments = filter_by_class_name(fragments, filter[:className]) if filter[:className].present?
+
+      if filter[:field].present? && filter[:field][:name].present? && filter[:field][:value].present?
+        fragments = filter_by_field_name(fragments, filter[:field][:name], filter[:field][:value])
       end
 
-      if filter.className.present?
-        data = filter_by_class_name(filter.className)
-      end
-
-      if filter.fieldName.present? && filter.value.present?
-        data = filter_by_fieldName(filter.fieldName, filter.value)
-      end
-
-      data
+      fragments
     end
 
     private
 
     def format_fragments(fragments)
-      fragments.map do |fragment|
-        if Api::V1::Madmp::MadmpFragmentsPolicy.new(context[:current_user], fragment).show?
-          fragment
-        end
-      end.compact
+      fragments.select { |fragment| Api::V1::Madmp::MadmpFragmentsPolicy.new(context[:current_user], fragment).show? }
     end
 
-    def filter_by_grant_id(grant_ids)
+    def filter_by_grant_id(fragments, grant_ids)
       if grant_ids.is_a?(Hash) && grant_ids["regex"].present?
         regex = grant_ids["regex"].gsub(/\A\/|\/\z/, '')
-        fragments = MadmpFragment.where("data->>'grantId' ~* ?", regex)
+        fragments = fragments.where("data->>'grantId' ~* ?", regex)
       elsif grant_ids.is_a?(Array)
-        grant_ids = grant_ids.compact.uniq
-        fragments = MadmpFragment.where("data->>'grantId' IN (?)", grant_ids)
+        fragments = fragments.where("data->>'grantId' IN (?)", grant_ids.compact.uniq)
       end
-      format_fragments(fragments)
+      fragments
     end
 
-    def filter_by_class_name(class_name)
-      fragments = MadmpFragment.where(classname: class_name)
-      format_fragments(fragments)
+    def filter_by_class_name(fragments, class_name)
+      fragments.where(classname: class_name)
     end
 
-    def filter_by_fieldName(field_name, value)
-      fragments =  MadmpFragment.where("data->>? = ?", field_name, value)
-      format_fragments(fragments)
+    def filter_by_field_name(fragments, field_name, value)
+      fragments.where("data->>? = ?", field_name, value)
     end
   end
 end
--- engines/madmp_opidor/app/graphql/types/plan_filter_input_type.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  # PlanFilterInputType
-  class PlanFilterInputType < Types::BaseInputObject
-    argument :id, String, required: false
-    argument :title, String, required: false
-  end
-end
