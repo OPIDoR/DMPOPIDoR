6a11705f56f9b0c0ff554c65b52db2ed5231b043
wilmouths <wilmouthsteven@gmail.com>
2024-11-27 10:41:20 +0100

    fix: catch SMTP Errors

--- app/controllers/contact_us/contacts_controller.rb
+++ app/controllers/contact_us/contacts_controller.rb
@@ -15,12 +15,20 @@ module ContactUs
         render_new_page and return
       end
       if @contact.save
-        redirect_to(ContactUs.success_redirect || '/',
+        return redirect_to(ContactUs.success_redirect || contact_us_path,
                     notice: _('Contact email was successfully sent.'))
       else
         flash[:alert] = _('Unable to submit your request')
-        render_new_page
+        render_new_page and return
       end
+    rescue Net::SMTPFatalError => e
+      Rails.logger.error("SMTP Error: #{e.message}")
+      flash[:alert] = _('An unexpected error occurred while sending the email. Please try again later.')
+      render_new_page and return
+    rescue StandardError => e
+      Rails.logger.error("Mailer Error: #{e.message}")
+      flash[:alert] = _('An unexpected error occurred while sending the email. Please try again later.')
+      render_new_page and return
     end
     # rubocop:enable Metrics/AbcSize
     # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
@@ -34,10 +42,10 @@ module ContactUs
 
     def render_new_page
       case ContactUs.form_gem
-      when 'formtastic'  then render 'new_formtastic'
-      when 'simple_form' then render 'new_simple_form'
+      when 'formtastic'  then render 'new_formtastic', locals: { contact: @contact }
+      when 'simple_form' then render 'new_simple_form', locals: { contact: @contact }
       else
-        render 'new'
+        render 'new', locals: { contact: @contact }
       end
     end
   end
