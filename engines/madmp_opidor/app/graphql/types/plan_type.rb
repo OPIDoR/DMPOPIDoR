7b11234f9b8288b173e093d736ae1c4a96865888
wilmouths <wilmouthsteven@gmail.com>
2024-12-04 11:24:28 +0100

    refactor: removing useless code

--- engines/madmp_opidor/app/graphql/types/plan_type.rb
+++ engines/madmp_opidor/app/graphql/types/plan_type.rb
@@ -4,17 +4,10 @@ module Types
   # PlanType
   class PlanType < Types::BaseObject
     field :plan_id, ID, null: false
-    # field :id, ID, null: false
     field :meta, GraphQL::Types::JSON, null: true
     field :project, GraphQL::Types::JSON, null: true
     field :researchOutput, GraphQL::Types::JSON, null: true
     field :researchEntity, GraphQL::Types::JSON, null: true
     field :budget, GraphQL::Types::JSON, null: true
-    # field :fragments, [Types::FragmentType], null: true, resolver: Resolvers::FragmentsResolver
-    # field :visibility, Boolean, null: false
-
-    # def fragments(object, filter: nil)
-    #  Resolvers::FragmentsResolver.new(object: object, context: context).resolve(filter: filter)
-    # end
   end
 end
