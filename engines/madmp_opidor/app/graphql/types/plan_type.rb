95b50b675f3ed5c32752cde4a668106447130897
wilmouths <wilmouthsteven@gmail.com>
2025-03-03 09:43:26 +0100

    refactor: remove type meta, projet & research_output, remove id & template name

--- engines/madmp_opidor/app/graphql/types/plan_type.rb
+++ engines/madmp_opidor/app/graphql/types/plan_type.rb
@@ -4,28 +4,29 @@ module Types
   # PlanType
   class PlanType < Types::BaseObject
     field :plan_id, ID, null: false
-    field :meta, Types::MetaType, null: true
-    field :project, Types::ProjectType, null: true
+    field :meta, GraphQL::Types::JSON, null: true
+    field :project, GraphQL::Types::JSON, null: true
     field :researchEntity, GraphQL::Types::JSON, null: true
     field :budget, GraphQL::Types::JSON, null: true
     field :template_name, String, null: true
-    field :researchOutput, GraphQL::Types::JSON, null: true do
-      argument :filter, Types::LogicalFilterInput, required: false
-      description 'Fetch research outputs'
-    end
+    field :researchOutput, GraphQL::Types::JSON, null: true
+    # field :researchOutput, GraphQL::Types::JSON, null: true do
+    #  argument :filter, Types::LogicalFilterInput, required: false
+    #  description 'Fetch research outputs'
+    # end
 
-    def researchOutput(filter: nil)
-      dmp_id = object["id"]
+    #def researchOutput(filter: nil)
+    # dmp_id = object["id"]
 
-      research_outputs = MadmpFragment.where(classname: 'research_output', dmp_id:)
+    # research_outputs = MadmpFragment.where(classname: 'research_output', dmp_id:)
 
-      return research_outputs.map { |research_output| research_output&.get_full_fragment(with_ids: true, with_template_name: true) } if filter.nil?
+    # return research_outputs.map { |research_output| research_output&.get_full_fragment(with_ids: true, with_template_name: true) } if filter.nil?
 
-      research_outputs.flat_map do |research_output|
-        Resolvers::ResearchOutputsFiltersResolver.apply(research_output.children, filter)&.map do |madmp_fragment|
-          madmp_fragment&.parent&.get_full_fragment(with_ids: true, with_template_name: true)
-        end
-      end.compact # rubocop:disable Style/MultilineBlockChain
-    end
+    # research_outputs.flat_map do |research_output|
+    #    Resolvers::ResearchOutputsFiltersResolver.apply(research_output.children, filter)&.map do |madmp_fragment|
+    #      madmp_fragment&.parent&.get_full_fragment(with_ids: true, with_template_name: true)
+    #    end
+    #  end.compact # rubocop:disable Style/MultilineBlockChain
+    #end
   end
 end
