495d4664747fac660a87c7c583ea72029dae0f65
wilmouths <wilmouthsteven@gmail.com>
2025-04-09 15:03:46 +0200

    feat: adding RDA format

--- engines/madmp_opidor/app/graphql/mutations/create_research_output.rb
+++ engines/madmp_opidor/app/graphql/mutations/create_research_output.rb
@@ -5,13 +5,21 @@ module Mutations
   class CreateResearchOutput < BaseMutation
     argument :plan_id, Int, required: true
     argument :research_outputs, GraphQL::Types::JSON, required: true
+    argument :format, String, default_value: 'dmp', required: false
 
     field :result, Types::MutationResponseType
 
-    def resolve(plan_id:, research_outputs:)
+    def resolve(plan_id:, research_outputs:, format:)
       plan = Api::V1::PlansPolicy::Scope.new(context[:current_user], Plan).resolve.find(plan_id)
 
       begin
+        if format.eql?('rda')
+          research_outputs = Import::Converters::RdaToStandardConverter.convert_research_output(research_outputs, {
+            ethical_issues_exist: 'no',
+            ethical_issues_description: '',
+            ethical_issues_report: ''
+          })
+        end
         Import::PlanImportService.handle_research_outputs(plan, research_outputs)
       rescue => e
           raise GraphQL::ExecutionError, e.message
@@ -20,7 +28,7 @@ module Mutations
       {
         result: {
           code: 200,
-          message: "Research output created successfully.",
+          message: "Research output#{research_outputs.length > 1 ? "s" : ""} created successfully for plan ${plan_id}.",
           success: true
         }
       }
