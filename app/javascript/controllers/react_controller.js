a98c6641520f2736a258ac35ccdcb425e55227b0
Benjamin FAURE <benjamin.faure@inist.fr>
2025-04-30 11:28:29 +0200

    fix: eslint syntax

--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -1,6 +1,6 @@
-import { Controller } from "@hotwired/stimulus"
-import React from "react"
-import ReactDOM from "react-dom/client"
+import { Controller } from '@hotwired/stimulus';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import NewsPageLayout from '../dmp_opidor_react/src/components/news/NewsPageLayout.jsx';
 import CommentList from '../dmp_opidor_react/src/components/Shared/CommentList.jsx';
@@ -28,27 +28,27 @@ const modules = {
   CookieConsent,
   BackToTop,
   SharedLabelLayout,
-}
+};
 
 export default class extends Controller {
   static values = {
     component: String,
-    props: Object
-  }
+    props: Object,
+  };
 
   connect() {
-    const module = modules[this.componentValue]
+    const module = modules[this.componentValue];
     if (module) {
-      this.root = ReactDOM.createRoot(this.element)
+      this.root = ReactDOM.createRoot(this.element);
       this.root.render(
-        React.createElement(module, this.propsValue)
-      )
+        React.createElement(module, this.propsValue),
+      );
     } else {
-      console.error(`Could not find module ${this.componentValue}`)
+      console.error(`Could not find module ${this.componentValue}`);
     }
   }
 
   disconnect() {
-    this.root.unmount()
+    this.root.unmount();
   }
 }
