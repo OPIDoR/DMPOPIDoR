830d2583363576f034a9e9be059fc38a1162ee64
Benjamin FAURE <benjamin.faure@inist.fr>
2025-05-13 14:38:35 +0200

    refactor: removed paginable controllers code into core codebase

--- app/controllers/paginable/guidance_groups_controller.rb
+++ app/controllers/paginable/guidance_groups_controller.rb
@@ -16,5 +16,14 @@ module Paginable
         format: :json
       )
     end
+
+    # GET /paginable/guidance_groups/publicly_visible/:page  (AJAX)
+    # -----------------------------------------------------
+    def publicly_visible
+      # We want the pagination/sort/search to be retained in the URL so redirect instead
+      # of processing this as a JSON
+      paginable_params = params.permit(:page, :search, :sort_field, :sort_direction)
+      redirect_to public_guidance_groups_path(paginable_params.to_h)
+    end
   end
 end
--- app/controllers/paginable/plans_controller.rb
+++ app/controllers/paginable/plans_controller.rb
@@ -72,5 +72,20 @@ module Paginable
         format: :json
       )
     end
+
+    # CHANGES: New Visibility
+    # /paginable/plans/administrator_visible/:page
+    # Paginable for Administrator Private Visibility
+    # Plans that are only visible by the owner of a plan, its collaborators and the org admin
+    def administrator_visible
+      raise Pundit::NotAuthorizedError unless Paginable::PlanPolicy.new(current_user).administrator_visible?
+
+      paginable_renderise(
+        partial: 'administrator_visible',
+        scope: ::Plan.org_admin_visible(current_user),
+        query_params: { sort_field: 'plans.updated_at', sort_direction: :desc },
+        format: :json
+      )
+    end
   end
 end
