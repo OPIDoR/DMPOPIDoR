cce5863f80fadfcdea5ab5939c878b241872a79d
Benjamin FAURE <benjamin.faure@inist.fr>
2025-08-29 13:02:03 +0200

    chore: annotated models

--- spec/factories/madmp_schemas.rb
+++ spec/factories/madmp_schemas.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: madmp_schemas
+#
+#  id            :integer          not null, primary key
+#  classname     :string
+#  data_type     :string           default("none"), not null
+#  label         :string
+#  name          :string
+#  schema        :json
+#  topics        :string           default(["standard"]), not null, is an Array
+#  version       :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  api_client_id :bigint(8)
+#  org_id        :integer
+#
+# Indexes
+#
+#  index_madmp_schemas_on_api_client_id  (api_client_id)
+#  index_madmp_schemas_on_org_id         (org_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (api_client_id => api_clients.id)
+#  fk_rails_...  (org_id => orgs.id)
+#
 FactoryBot.define do
   factory :madmp_schemas do
     data { {} }
