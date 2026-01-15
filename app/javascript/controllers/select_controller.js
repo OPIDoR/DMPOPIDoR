906912af3d568ee6b9d8774fae00148f2ea92e94
Benjamin FAURE <benjamin.faure@inist.fr>
2026-01-15 15:17:02 +0100

    fix: notification display following role update

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -1,14 +1,30 @@
-import { Controller } from '@hotwired/stimulus';
+import { Controller } from "@hotwired/stimulus"
+import * as notifier from '../src/utils/notificationHelper';
 
 export default class extends Controller {
+  static values = { url: String }
+
   update(event) {
-    fetch(this.element.dataset.url, {
-      method: 'PATCH',
+    const value = event.target.value
+
+    fetch(this.urlValue, {
+      method: "PATCH",
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'text/vnd.turbo-stream.html',
+        "Content-Type": "application/json",
+        "Accept": "application/json"
       },
-      body: JSON.stringify({ role: { access: event.target.value } }),
-    });
+      body: JSON.stringify({ role: { access: value } })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.code === 1) {
+          notifier.renderNotice(data.msg)
+        } else {
+          notifier.renderAlert(data.msg)
+        }
+      })
+      .catch(() => {
+        notifier.renderAlert("Erreur réseau")
+      })
   }
 }
