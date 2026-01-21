5d89e90d9f64d75f4a627a1a67995c938f9511a0
wilmouths <wilmouthsteven@gmail.com>
2026-01-21 15:35:51 +0100

    fix: adding gin index on data column

--- db/migrate/20260119131312_create_json_plans.rb
+++ db/migrate/20260119131312_create_json_plans.rb
@@ -9,6 +9,6 @@ class CreateJsonPlans < ActiveRecord::Migration[8.1]
       t.timestamps
     end
 
-    add_index :json_plans, :dmp_id, :data, using: :gin
+    add_index :json_plans, :dmp_id
   end
 end
