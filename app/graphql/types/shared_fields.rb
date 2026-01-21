ab6aa85e228af5c42f054a519bb51b6514f83f04
wilmouths <wilmouthsteven@gmail.com>
2026-01-21 16:17:26 +0100

    refactor: remove subfilter (not needed)

--- app/graphql/types/shared_fields.rb
+++ app/graphql/types/shared_fields.rb
@@ -2,14 +2,13 @@
 #
 module Types
   module SharedFields
-    def self.shared_and_or_filter(level: 0)
+    def self.shared_and_or_filter
       Proc.new do
         # argument :className, Types::ClassnameEnum, required: false, description: 'Specifies the class name of the fragment to filter.'
         argument :field, String, required: false, description: 'The field to apply the filter on.'
         argument :value, GraphQL::Types::JSON, required: false, description: 'The value to compare against the specified field.'
         argument :operator, String, required: false, description: 'The comparison operator to use (e.g., eq for equals, neq for not equals, gt for greater than, lt for less than, etc.).'
-        argument :filter, -> { Types::LogicalFilterInput }, required: false, description: 'A nested filter to allow complex filtering conditions.' if level == 0
-        argument :filter, -> { Types::LogicalSubfilterInput }, required: false, description: 'A nested filter to allow complex filtering conditions.' if level == 1
+        argument :filter, -> { Types::LogicalFilterInput }, required: false, description: 'A nested filter to allow complex filtering conditions.'
       end
     end
 
