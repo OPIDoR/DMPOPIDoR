edf32ba1a6d5cca98f0a824b8f1cf0b63844d0b8
Benjamin FAURE <benjamin.faure@inist.fr>
2026-02-03 15:38:16 +0100

    chore: cleaned unused files

--- app/javascript/channels/consumer.js
+++
@@ -1,6 +0,0 @@
-// By default, ActionCable channels use the App.cable consumer created in the
-// app/assets/javascripts/cable.js file. To avoid polluting the global namespace
-// we use webpack’s module system to provide a consumer for our channels.
-import { createConsumer } from '@rails/actioncable';
-
-export default createConsumer();
