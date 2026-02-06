a162cc0d4de3a809f2a3bc9a8f831add1e896394
Benjamin FAURE <benjamin.faure@inist.fr>
2026-02-06 09:49:38 +0100

    refactor: removed mini css extract plugin

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,79 +1,68 @@
-const path = require('path');
-const webpack = require('webpack');
-const { EsbuildPlugin } = require('esbuild-loader');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const { EsbuildPlugin } = require("esbuild-loader");
 
-const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+const mode =
+  process.env.NODE_ENV === "development" ? "development" : "production";
 
 module.exports = {
   mode,
-  devtool: mode === 'development' ? 'eval-cheap-module-source-map' : 'source-map',
+  devtool:
+    mode === "development" ? "eval-cheap-module-source-map" : "source-map",
   module: {
     rules: [
       // Use esbuild to compile JavaScript & TypeScript
       {
         // Match `.js`, `.jsx`, `.ts` or `.tsx` files
         test: /\.[jt]sx?$/,
-        loader: 'esbuild-loader',
+        loader: "esbuild-loader",
         options: {
           // JavaScript version to compile to
-          target: 'es2015',
+          target: "es2015",
         },
       },
       {
         test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'esbuild-loader',
-            options: {
-              minify: true,
-            },
-          },
-        ],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpe?g|gif|eot|woff2|woff|ttf|svg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   entry: {
-    application: './app/javascript/application.js',
+    application: "./app/javascript/application.js",
   },
   optimization: {
     minimizer: [
       new EsbuildPlugin({
-        target: 'es2015',
-        css: true,
-      })
-    ]
+        target: "es2015",
+      }),
+    ],
   },
   output: {
-    filename: '[name].js',
-    sourceMapFilename: '[file].map',
-    path: path.resolve(__dirname, '..', '..', 'app/assets/builds'),
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    path: path.resolve(__dirname, "..", "..", "app/assets/builds"),
   },
   plugins: [
-    new MiniCssExtractPlugin(),
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1,
     }),
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      'global.jQuery': 'jquery',
-      React: 'react',
-      ReactDOM: 'react-dom',
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      "global.jQuery": "jquery",
+      React: "react",
+      ReactDOM: "react-dom",
     }),
   ],
   resolve: {
-    extensions: ['.*', '.js', '.jsx'],
+    extensions: [".*", ".js", ".jsx"],
     alias: {
-      react: path.resolve('./node_modules/react'),
+      react: path.resolve("./node_modules/react"),
     },
   },
 };
