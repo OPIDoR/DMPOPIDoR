d386e42e8677c687456a066860d749d92ca42cbe
Benjamin FAURE <benjamin.faure@inist.fr>
2026-03-10 17:12:41 +0100

    refactor: added tooltip in saved guidance component

--- app/javascript/src/components/GuidanceChoice/SavedGuidances.jsx
+++ app/javascript/src/components/GuidanceChoice/SavedGuidances.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
-import { FaEye } from 'react-icons/fa6';
+import { FaEye, FaCircleInfo } from 'react-icons/fa6';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
 import { GlobalContext } from '../context/Global';
 
 function SavedGuidances() {
@@ -12,7 +13,22 @@ function SavedGuidances() {
     <>
       {savedGuidances.length > 0 && (
         <div style={{ paddingLeft: '20px', flex: 1 }}>
-          <h3 style={{fontWeight: "bold"}}>{t('followingGuidancesApplyToThisResearchOutput')}</h3>
+          <h3 style={{fontWeight: "bold"}}>
+            {t('followingGuidancesApplyToThisResearchOutput')}
+            <ReactTooltip
+              id="saved-guidances-info-tooltip"
+              place="bottom"
+              effect="solid"
+              variant="info"
+              content={t("savedGuidancesInfo")}
+              style={{ width: "30%" }}
+            />
+            <FaCircleInfo
+              data-tooltip-id="saved-guidances-info-tooltip"
+              size={18}
+              style={{ paddingLeft: "5px" }}
+            />
+          </h3>
           <ul>
             {savedGuidances.map((guidance) => (
               <li key={guidance.id}>
