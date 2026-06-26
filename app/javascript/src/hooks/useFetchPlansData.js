85ac70489ae7b3f6e07b2b456f5c643f5f0af904
Benjamin FAURE <benjamin.faure@inist.fr>
2026-06-26 16:38:21 +0200

    refactor: added dataType support to ResearchOutputSelector

--- app/javascript/src/hooks/useFetchPlansData.js
+++ app/javascript/src/hooks/useFetchPlansData.js
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
 import { researchOutput } from "../services";
 
-function useFetchPlansData() {
+function useFetchPlansData(dataType = null) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     researchOutput.getPlans().then(({ data }) => {
-      setData(data);
+      /* filtering plans and research outputs based on dataType */
+      const filteredPlans = dataType
+        ? data.plans
+            .filter((p) =>
+              p.research_outputs.some((pr) => pr.output_type == dataType),
+            )
+            .map((p) => ({
+              ...p,
+              research_outputs: p.research_outputs.filter(
+                (pr) => pr.output_type == dataType,
+              ),
+            }))
+        : data.plans;
+      setData(filteredPlans);
       setLoading(false);
     });
   }, []);
