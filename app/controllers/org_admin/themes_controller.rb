57995d48f2bb4a13b79dd40e2b102bb8af0c686c
Benjamin FAURE <benjamin.faure@inist.fr>
2026-07-23 17:15:34 +0200

    refactor: updated themes controller for locale & data_type support

--- app/controllers/org_admin/themes_controller.rb
+++ app/controllers/org_admin/themes_controller.rb
@@ -4,9 +4,11 @@ module OrgAdmin
   # Controller that handles themes rendering
   class ThemesController < ApplicationController
     def index
+      locale = params[:locale]
+      data_types = params[:data_types]&.split(',') || 'dataset'
       render json: {
-        themes: Theme.all.order('title').map do |theme|
-          { id: theme.id, title: theme.title }
+        themes: Theme.where(data_type: data_types).order('title').map do |theme|
+          { id: theme.id, title: theme&.translations&.[](locale)&.fetch('title', nil) || theme&.title }
         end
       }
     end
