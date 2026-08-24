02bfc94391e67147125324ea5828e451444478fb
Benjamin FAURE <benjamin.faure@inist.fr>
2026-08-24 13:58:37 +0200

    chore: added vitest lib & config

---
+++ app/javascript/src/vitest.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    root: path.resolve(__dirname),
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./setupTests.js",
+    css: true,
+  },
+});
