ef73e3a1f744f82cb9baebe476dc9337b2545e0e
Benjamin FAURE <benjamin.faure@inist.fr>
2026-09-08 11:21:27 +0200

    fix: json_plan should not be generated if plan has not been modified

--- app/jobs/json_plan_job.rb
+++ app/jobs/json_plan_job.rb
@@ -8,7 +8,9 @@ class JsonPlanJob < ApplicationJob
     plan = Plan.find_by(id: plan_id)
     return unless plan
 
-    json_plan = JsonPlan.find_or_initialize_by(plan: plan)
+    json_plan = JsonPlan.find_or_initialize_by(plan_id:)
+    return if plan.updated_at <= json_plan.updated_at
+
     json_plan.assign_attributes(
       dmp_id: plan.json_fragment.id,
       research_outputs_uuids: plan.research_outputs.pluck(:uuid),
