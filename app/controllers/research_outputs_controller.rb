5060a3a8544d8bdf3fdb7affd4b40d676e63236e
Benjamin FAURE <benjamin.faure@inist.fr>
2026-09-24 17:49:55 +0200

    refactor(viewed_comment): added optimization for viewed comment data loading when fetching RO data

--- app/controllers/research_outputs_controller.rb
+++ app/controllers/research_outputs_controller.rb
@@ -9,7 +9,7 @@ class ResearchOutputsController < ApplicationController
   after_action :verify_authorized
 
   def show
-    @research_output = ResearchOutput.includes(answers: [:madmp_schema],
+    @research_output = ResearchOutput.includes(answers: %i[madmp_fragment viewed_comment],
                                                plan: { template: { phases: { sections: :questions } } })
                                      .find(params[:id])
     authorize @research_output
